9aeb95a6a8f978752428b1e60602886d485f7028
Muhammet ESER <muhammeteser29@gmail.com>
2017-12-10 14:27:24 +0300

    yapı jquery'e geçirildi

--- js/app.js
+++ js/app.js
@@ -1,71 +1,38 @@
-document.querySelector('.start').addEventListener("click", () => {
-
+$('.start').click(() => {
     let counter = setInterval(() => {
-        let presentTime = document.getElementById('timer').innerHTML;
+        let presentTime = $('#timer').text();
         let timeArray = presentTime.split(/[:]+/);
         let m = timeArray[0];
         let s = checkSecond((timeArray[1] - 1));
         if (s == 59) { m = m - 1 }
         if (m <= 0 && s == 0) {
-            m = "05";
-            s = "00";
+            m = "05"; s = "00";
             clearInterval(counter);
-            document.getElementById('music').play();
-            document.getElementById('timer').innerHTML = m + ":" + s;
+            $('#music').play();
+            $('#timer').text(m + ":" + s);
         }
-
-        document.getElementById('timer').innerHTML = m + ":" + s;
+        $('#timer').text(m + ":" + s);
     }, 1000);
-
     function checkSecond(sec) {
-        if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10
+        if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10 
         if (sec < 0) { sec = "59" };
         return sec;
     }
-
-    document.querySelector('.stop').addEventListener("click", () => {
+    $('.stop').click(() => {
         clearInterval(counter);
-
     });
-
-
-    document.querySelector('.pomodoro').addEventListener("click", () => {
+    $('.pomodoro').click(() => {
         clearInterval(counter);
-        document.getElementById('timer').innerHTML = "25:00";
-
+        $('#timer').text("25:00");
     });
-
-    document.querySelector('.reset').addEventListener("click", () => {
+    $('.reset').click(() => {
         clearInterval(counter);
-        document.getElementById('timer').innerHTML = "25:00";
-
+        $('#timer').text("25:00");
     });
-
-
-
-
 });
-
-document.querySelector('.long').addEventListener("click", () => {
-
-    document.getElementById('timer').innerHTML = "30:00";
-
-
+$('.long').click(() => {
+    $('#timer').text("30:00");
 });
-
-document.querySelector('.short').addEventListener("click", () => {
-
-    document.getElementById('timer').innerHTML = "05:00";
-
-
+$('.short').click(() => {
+    $('#timer').text("05:00");
 });
-
-
-
-
-
-
-
-
-
-
